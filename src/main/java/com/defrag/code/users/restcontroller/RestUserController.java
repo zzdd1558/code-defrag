8ec161ce909cef92fb5d4403f6a83137b4cca405
yunjin choi <zzdd1558@naver.com>
2019-05-18 15:38:03 +0900

    RequestMapping 중복 제거

--- src/main/java/com/defrag/code/users/restcontroller/RestUserController.java
+++ src/main/java/com/defrag/code/users/restcontroller/RestUserController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,9 +24,9 @@ public class RestUserController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	@RequestMapping(value = "/{userPk}", method = RequestMethod.GET)
+	public String home(Locale locale, Model model , @PathVariable int userPk) {
+		System.out.println("RestController User PK : " + userPk);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
