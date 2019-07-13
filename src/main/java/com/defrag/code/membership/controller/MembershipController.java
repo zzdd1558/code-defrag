0725ef171dfbeb94e1f4f0e05896fd97bf161168
yunjin choi <zzdd1558@naver.com>
2019-07-13 15:00:05 +0900

    commit

---
+++ src/main/java/com/defrag/code/membership/controller/MembershipController.java
@@ -0,0 +1,48 @@
+package com.defrag.code.membership.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.defrag.code.membership.MembershipVO;
+import com.defrag.code.membership.service.MembershipService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/membership")
+public class MembershipController {
+	
+	@Resource(name="memberShipService")
+	private MembershipService memberShipService;
+	
+	/**
+	 * 회원가입 페이지
+	 * @return
+	 */
+	@RequestMapping(value = "/membershipJoin", method = RequestMethod.GET)
+	public String membership() {
+		String view ="회원가입";
+		
+		return view;
+	}
+	
+	/**
+	 * 회원 정보 INSERT
+	 * @return
+	 */
+	@RequestMapping(value = "/membershipTrans", method = RequestMethod.POST)
+	public String membershipTrans(MembershipVO membershipvo) {
+		
+		int result = memberShipService.membershipTrans(membershipvo);
+		
+		if(result == 1) {
+			return "성공";	
+		} else {
+			return "실패";
+		}
+	}
+}
--- src/main/java/com/defrag/code/membership/dao/MembershipDAO.java
+++ src/main/java/com/defrag/code/membership/dao/MembershipDAO.java
@@ -3,13 +3,13 @@ package com.defrag.code.membership.dao;
 import org.springframework.stereotype.Repository;
 
 import com.defrag.code.base.dao.AbstractDAO;
-import com.defrag.code.membership.JoinVO;
+import com.defrag.code.membership.MembershipVO;
 
 @Repository("MemberShipDAO")
 public class MembershipDAO extends AbstractDAO {
 
-	public int joinTrans(JoinVO joinvo) {
-		return insert("com.defrag.code.membership.insertMemberShip", joinvo);
+	public int membershipTrans(MembershipVO membershipvo) {
+		return insert("com.defrag.code.membership.insertMemberShip", membershipvo);
 	}
 	
 }
